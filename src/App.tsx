e6435d6d4f74ae34afee99cb911e40c49d92e35b
SergeiVS <fam.smirnoff@gmail.com>
2024-07-30 21:47:24 +0200

    some commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import GlobalStyles from "styles/GlobalStyles"
 
 function App() {
-  return <>Weather App</>
+  return (
+    <>
+      {/* <Layout_Team_1>
+        <Routes>
+          <Route path={PagesPaths.HOME}></Route>
+          <Route
+            path={PagesPaths.CREATE_EMPLOYEES}
+            element={<CreateEmployee />}
+          />
+          <Route path={PagesPaths.EMPLOYEES} element={<Employees />} />
+          <Route path="*" element="Page not found" />
+        </Routes>
+      </Layout_Team_1> */}
+    </>
+  )
 }
 
 export default App
