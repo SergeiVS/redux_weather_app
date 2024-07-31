2ca4221d29c3854f90d756bca2d8e28ff5ea2db6
SergeiVS <fam.smirnoff@gmail.com>
2024-07-31 20:52:43 +0200

    merge changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,7 @@ import { PageWrapper } from "./styles"
 import WeatherCard from "components/WeatherCard/WeatherCard"
 
 function Home() {
-    return <PageWrapper>
-      <WeatherCard />
-  </PageWrapper>
+  return <PageWrapper>{/* <WeatherCard /> */}</PageWrapper>
 }
 
 export default Home
