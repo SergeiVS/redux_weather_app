9de4640b2580543dc2747f6cf6409d6e13b16bf8
“Elvira55872” <elli-x1996@mail.ru>
2024-07-31 18:59:57 +0200

    added Weather Card

---
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -0,0 +1,30 @@
+import {
+  ButtonsContainer,
+  City,
+  Degrees,
+  ResponseContainer,
+  ResultContainer,
+  WeatherCardContainer,
+  WeatherPicture,
+} from "./styles"
+import Button from "components/Button/Button"
+
+function WeatherCard() {
+  return (
+    <WeatherCardContainer>
+      <ResponseContainer>
+        <ResultContainer>
+          <Degrees>18</Degrees>
+          <City>LA</City>
+        </ResultContainer>
+        <WeatherPicture src="https://png.pngtree.com/png-clipart/20230315/ourmid/pngtree-sun-cartoon-cute-doodle-summer-png-image_6646856.png"></WeatherPicture>
+      </ResponseContainer>
+      <ButtonsContainer>
+        <Button name="Save" isTransparent/>
+        <Button name="Delete" isTransparent/>
+      </ButtonsContainer>
+    </WeatherCardContainer>
+  )
+}
+
+export default WeatherCard
